59b71e53b52eb0965e03d6e0bc50da93a9e12ad6
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-07 17:48:44 -0400

    seeded accounts data and created first CRUD endpoint

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.json(accounts);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Failed to get accounts' });
+        });
+});
+
 module.exports = server;
